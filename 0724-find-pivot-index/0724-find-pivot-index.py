a8e79e3e57df11e092f82111bade527a2bf2c40f
Tallapalli Madhu Sudhana <121922235+madhu14t@users.noreply.github.com>
2023-06-23 23:16:14 +0530

    Time: 195 ms (21.81%), Space: 17.7 MB (28.36%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -7,15 +7,8 @@ class Solution:
         s1=0
         for i in range(len(nums)):
             s1+=nums[i]/2
-            # print('hai',s1)
-            # if(nums[i]%2==0):
             if(s==s1):
                 return i
             s1+=nums[i]/2
-            # else:
-            #     if(s1==s):
-            #         # print('hi')
-            #         return i
-            #     s1+=(nums[i]/2)
             print(s1)
         return -1
