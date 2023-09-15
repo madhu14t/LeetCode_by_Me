63a62cde910e24474d76f34b7394d0012897092f
Tallapalli Madhu Sudhana <121922235+madhu14t@users.noreply.github.com>
2023-09-15 18:53:59 +0530

    Time: 2 ms (63.05%), Space: 44.9 MB (85.59%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zeroIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[zeroIndex];
+                nums[zeroIndex] = temp;
+                zeroIndex++;
+            }
+        }
+    }
+}
