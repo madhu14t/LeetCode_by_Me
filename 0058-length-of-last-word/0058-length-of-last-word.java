a5243c0de6ec66fdde0dbe83c888c081e6ca8e71
Tallapalli Madhu Sudhana <121922235+madhu14t@users.noreply.github.com>
2023-10-09 22:14:21 +0530

    Time: 0 ms (100%), Space: 41.2 MB (7%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count;
+        count=0;
+        String str= s.trim();
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!=' '){
+                count ++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
