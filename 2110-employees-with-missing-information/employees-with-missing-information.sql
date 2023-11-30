ac55ead8944a6b319f78ec56c5b42ce17e9ae051
Tallapalli Madhu Sudhana <121922235+madhu14t@users.noreply.github.com>
2023-11-30 11:07:07 +0530

    Time: 932 ms (95.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,2 @@
+select employee_id from salaries where employee_id not in (select employee_id from employees) union select employee_id from employees where employee_id not in 
+(select employee_id from salaries) order by employee_id;
