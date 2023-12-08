d86859ec7c77ddd7ddc520e8e79486b2260a869a
Tallapalli Madhu Sudhana <121922235+madhu14t@users.noreply.github.com>
2023-12-08 22:31:48 +0530

    Time: 4 ms (77.00%) | Memory: 10.7 MB (37.42%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for (int num : arr) {
+            if (s.count(num * 2) || (num % 2 == 0 && s.count(num / 2))) {
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+    }
+};
