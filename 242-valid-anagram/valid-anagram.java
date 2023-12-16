05670b7d470643fc67879cd22a32a53c07d350a1
Tallapalli Madhu Sudhana <121922235+madhu14t@users.noreply.github.com>
2023-12-16 21:18:13 +0530

    Time: 3 ms (89.36%) | Memory: 44 MB (17.96%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] schar=s.toCharArray();
+        char[] tchar=t.toCharArray();
+        Arrays.sort(schar);
+        Arrays.sort(tchar);
+        return Arrays.equals(schar,tchar);
+
+        
+    }
+}
